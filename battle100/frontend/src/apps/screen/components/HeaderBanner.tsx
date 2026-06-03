c4e59c58d17c6e8fbcf92addc4a2ce862ef4a475
quangbinh300986 <quangbinh300986@gmail.com>
2026-06-03 22:30:00 +0800

    视觉优化：重命名大屏 Logo 为 logo_shield_v2.png 以强制清除浏览器缓存

--- battle100/frontend/src/apps/screen/components/HeaderBanner.tsx
+++ battle100/frontend/src/apps/screen/components/HeaderBanner.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import logoShield from '../../../assets/logo_shield.png'
+import logoShield from '../../../assets/logo_shield_v2.png'
 
 interface HeaderBannerProps {
   theme?: string
